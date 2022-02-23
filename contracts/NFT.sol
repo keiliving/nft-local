d70b1562c29dc07eafa3dd14e3e84c34e337ad29
keiliving <keiliving6@gmail.com>
2022-02-23 16:37:42 +0900

    create contract and test

---
+++ contracts/NFT.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+//https://ethereum.stackexchange.com/questions/111568/hardhat-console-cant-be-resolved-in-vscode/114434#114434
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+contract NFT is ERC721PresetMinterPauserAutoId {
+  constructor() ERC721PresetMinterPauserAutoId("NFT TesT", "NFTTT", "https://hogehoge/api/v1/tokens/") {}
+}
