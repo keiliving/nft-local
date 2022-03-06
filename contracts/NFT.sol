967f72d3d19f194ead636861df4550bd21370214
keiliving <keiliving6@gmail.com>
2022-03-06 14:47:21 +0900

    add test for Ownable

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 //https://ethereum.stackexchange.com/questions/111568/hardhat-console-cant-be-resolved-in-vscode/114434#114434
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
-contract NFT is ERC721PresetMinterPauserAutoId {
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract NFT is ERC721PresetMinterPauserAutoId, Ownable {
   constructor() ERC721PresetMinterPauserAutoId("NFT TesT", "NFTTT", "https://hogehoge/api/v1/tokens/") {}
 }
